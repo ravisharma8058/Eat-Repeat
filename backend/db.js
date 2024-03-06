81b2db132fa2a52b5bfb0f7dcd22ef2e9838a9cf
ravisharma8058 <sharmaravi252002@gmail.com>
2024-03-06 23:13:52 +0530

    new commit

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 // const mongoDbClient = require("mongodb").MongoClient
-const mongoURI = 'mongodb+srv://sharmaravi252002:gNfgeWEK7O03TcM8@cluster0.ufdm3hd.mongodb.net/?retryWrites=true&w=majority' // Customer change url to your db you created in atlas
+const mongoURI = 'mongodb+srv://sharmaravi252002:F4wkacr7YiVDFyfU@cluster0.ufdm3hd.mongodb.net/?retryWrites=true&w=majority' // Customer change url to your db you created in atlas
 // mongodb://<username>:<password>@merncluster-shard-00-00.d1d4z.mongodb.net:27017,merncluster-shard-00-01.d1d4z.mongodb.net:27017,merncluster-shard-00-02.d1d4z.mongodb.net:27017/?ssl=true&replicaSet=atlas-eusy5p-shard-0&authSource=admin&retryWrites=true&w=majority
 module.exports = function (callback) {
     mongoose.connect(mongoURI, { useNewUrlParser: true }, async (err, result) => {
@@ -11,9 +11,10 @@ module.exports = function (callback) {
             console.log("connected to mongo")
             const foodCollection = await mongoose.connection.db.collection("food_items");
             foodCollection.find({}).toArray(async function (err, data) {
-                const categoryCollection = await mongoose.connection.db.collection("Categories");
+                const categoryCollection = await mongoose.connection.db.collection("foodCategory");
                 categoryCollection.find({}).toArray(async function (err, Catdata) {
                     callback(err, data, Catdata);
+                    console.log(data);
 
                 })
             });
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -87,6 +87,7 @@ export default function Home() {
               )
             })
             : ""}
+       {/* <div>hello world</div> */}
       </div>
       <Footer />
     </div>
